eb00b3b5e6bdc1679a8d7c095cd312eb3ec9a0f1
maelblancoperez-star <maelblancoperez@lasallesanasensio.es>
2026-01-19 12:06:40 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let sonar = 0
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    sonar = CutebotPro.ultrasonic(SonarUnit.Centimeters)
+    sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
     if (sonar > 2 && sonar < 20) {
         CutebotPro.trolleySteering(CutebotProTurn.LeftInPlace, 180)
     } else {
